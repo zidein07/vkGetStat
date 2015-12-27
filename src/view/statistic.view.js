420e3440a773c41eabbce61bc8ea3f78cf4a4369
zidein07 <zidein07@gmail.com>
2015-12-27 20:35:26 +0400

    chart data

--- src/view/statistic.view.js
+++ src/view/statistic.view.js
@@ -3,12 +3,15 @@ var Backbone = Backbone || {};
 
 app.statisticView = Backbone.View.extend({
   el: '.container',
-  events: {},
+  events: {
+    'click #chartDataGroup': 'getIdGroup'
+  },
   initialize: function () {
 
   },
   render: function () {
     var dataForChart = [];
+    var self = this;
     this.token = app.token.getToken();
     this.fetchWallData().then(function (response) {
       response.forEach(function (item) {
@@ -20,17 +23,16 @@ app.statisticView = Backbone.View.extend({
           dataForChart[dateStr] = [];
         }
         dataForChart[dateStr].push(item);
-//        console.log(dataForChart[dateStr]);
       });
-      console.log('dataForChart', dataForChart);
-
+      self.chart(dataForChart);
     });
   },
   fetchWallData: function () {
     var dataStat = [];
     var url = app.vk.getWall({
       owner_id: '-51313495',
-      count: 100
+      count: 100,
+      offset: 0
     });
     return url.then(function (response) {
       var dataResponse = response.response.splice(1);
@@ -40,6 +42,29 @@ app.statisticView = Backbone.View.extend({
         });
       });
       return dataStat;
+    });
+  },
+  chart: function (data) {
+    console.log('data', data);
+    var category = [];
+    var columnData = ['data1'];
+    for (var item in data) {
+      columnData.push(data[item].length);
+      category.push(item);
+    }
+    var chart = c3.generate({
+      bindto: '.statistic',
+      data: {
+        columns: [
+          columnData
+        ]
+      },
+      axis: {
+        x: {
+          type: 'category',
+          categories: category
+        }
+      }
     });
   }
 });
